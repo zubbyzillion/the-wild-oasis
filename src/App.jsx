ec8774e47be93230039eadd87a0ae0174554b182
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2024-09-04 02:55:27 +0100

    Done setting up routes and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,33 @@
-import styled from "styled-components";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
 import GlobalStyles from "./styles/GlobalStyles";
-import Button from "./ui/Button";
-import Input from "./ui/Input";
-import Heading from "./ui/Heading";
-import Row from "./ui/Row";
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+
 
 function App() {
-	const StyledApp = styled.main`
-		/* background-color: orangered; */
-		padding: 20px;
-	`;
-
 	return (
 		<>
 			<GlobalStyles />
-			<StyledApp>
-				<Row>
-					<Row type="horizontal">
-						<Heading as="h1">The Wild Oasis</Heading>
-
-						<div>
-							<Heading as="h2">Check In and Out</Heading>
-							<Button onClick={() => alert("Checked In")}>Check In</Button>
-							<Button variation="secondary" size="small" onClick={() => alert("Checked Out")}>Check Out</Button>
-						</div>
-					</Row>
-
-					<Row type="vertical">
-						<Heading as="h3">Form</Heading>
-						<form>
-							<Input type="number" placeholder="Number of Guests" />
-							<Input type="number" placeholder="Number of Guests" />
-						</form>
-					</Row>
-				</Row>
-			</StyledApp>
+		<BrowserRouter>
+			<Routes>
+				<Route index element={<Navigate replace to="dashboard" />} />
+				<Route path="dashboard" element={<Dashboard />} />
+				<Route path="bookings" element={<Bookings />} />
+				<Route path="cabins" element={<Cabins />} />
+				<Route path="users" element={<Users />} />
+				<Route path="settings" element={<Settings />} />
+				<Route path="account" element={<Account />} />
+				<Route path="login" element={<Login />} />
+				<Route path="*" element={<PageNotFound />} />
+			</Routes>
+		</BrowserRouter>
 		</>
 	);
 }
--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -9,7 +9,7 @@ const Heading = styled.h1`
     ${(props) => props.as === "h1" && css`
         font-size: 3rem;
         font-weight: 600;
-        background-color: yellow;
+        /* background-color: yellow; */
     `}
 
     ${(props) => props.as === "h2" && css`
