7a17bd6e0df3b9cc63b5b9ce1f19675c19690b50
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2024-09-12 00:40:01 +0100

    Mutation: Deleting Cabins

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -10,4 +10,21 @@ export async function getCabins() {
 
 	return data;
 }
+
+export async function deleteCabin(id) {
+	
+	const { data, error } = await supabase
+		.from('cabins')
+		.delete()
+		.eq("id", id);
+
+		if (error) {
+			console.error(error);
+			throw new Error("Cabins could not be deleted");
+		}
+
+	return data;
+
+}
+
 // https://klaxcklnibrbaycnmuve.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg
