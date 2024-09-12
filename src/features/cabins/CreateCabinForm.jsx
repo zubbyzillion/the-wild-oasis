92649bc871c2d7df72e629533dd1f069c919dae3
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2024-09-12 11:19:13 +0100

    Resolved handling fomr erros - slight issues

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -9,103 +9,165 @@ import { useForm } from "react-hook-form";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createCabin } from "../../services/apiCabins";
 import toast from "react-hot-toast";
+import FormRow from "../../ui/FormRow";
 
-const FormRow = styled.div`
-  display: grid;
-  align-items: center;
-  grid-template-columns: 24rem 1fr 1.2fr;
-  gap: 2.4rem;
+const FormRow2 = styled.div`
+	display: grid;
+	align-items: center;
+	grid-template-columns: 24rem 1fr 1.2fr;
+	gap: 2.4rem;
 
-  padding: 1.2rem 0;
+	padding: 1.2rem 0;
 
-  &:first-child {
-    padding-top: 0;
-  }
+	&:first-child {
+		padding-top: 0;
+	}
 
-  &:last-child {
-    padding-bottom: 0;
-  }
+	&:last-child {
+		padding-bottom: 0;
+	}
 
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
+	&:not(:last-child) {
+		border-bottom: 1px solid var(--color-grey-100);
+	}
 
-  &:has(button) {
-    display: flex;
-    justify-content: flex-end;
-    gap: 1.2rem;
-  }
+	&:has(button) {
+		display: flex;
+		justify-content: flex-end;
+		gap: 1.2rem;
+	}
 `;
 
 const Label = styled.label`
-  font-weight: 500;
+	font-weight: 500;
 `;
 
 const Error = styled.span`
-  font-size: 1.4rem;
-  color: var(--color-red-700);
+	font-size: 1.4rem;
+	color: var(--color-red-700);
 `;
 
 function CreateCabinForm() {
-  const queryClient = useQueryClient();
+	const queryClient = useQueryClient();
 
-  const { mutate, isLoading: isCreating } = useMutation({
-    mutationFn: createCabin,
-    onSuccess: () => {
-      toast.success("New cabin successfully created");
-      queryClient.invalidateQueries({ queryKey: ["cabins"] });
-      reset();
-    },
-    onError: (err) => toast.error(err.message),
-  });
+	const { mutate, isLoading: isCreating } = useMutation({
+		mutationFn: createCabin,
+		onSuccess: () => {
+			toast.success("New cabin successfully created");
+			queryClient.invalidateQueries({ queryKey: ["cabins"] });
+			reset();
+		},
+		onError: (err) => toast.error(err.message),
+	});
 
-  const { register, handleSubmit, reset } = useForm();
-  
-  function onSubmit(data) {
-    mutate(data);
-  }
+	const { register, handleSubmit, reset, getValues, formState } = useForm();
 
-  return (
-    <Form onSubmit={handleSubmit(onSubmit)}>
-      <FormRow>
-        <Label htmlFor="name">Cabin name</Label>
-        <Input type="text" id="name" {...register("name")} />
-      </FormRow>
+	const { errors } = formState;
+	console.log(errors);
 
-      <FormRow>
-        <Label htmlFor="maxCapacity">Maximum capacity</Label>
-        <Input type="number" id="maxCapacity" {...register("maxCapacity")} />
-      </FormRow>
+	function onSubmit(data) {
+		mutate(data);
+	}
 
-      <FormRow>
-        <Label htmlFor="regularPrice">Regular price</Label>
-        <Input type="number" id="regularPrice" {...register("regularPrice")} />
-      </FormRow>
+	function onError(errors) {
+		console.log(errors);
+	}
 
-      <FormRow>
-        <Label htmlFor="discount">Discount</Label>
-        <Input type="number" id="discount" defaultValue={0} {...register("discount")} />
-      </FormRow>
+	return (
+		<Form onSubmit={handleSubmit(onSubmit, onError)}>
+			<FormRow2>
+				<Label htmlFor="name">Cabin name</Label>
+				<Input
+					type="text"
+					id="name"
+					{...register("name", {
+						required: "This field is required",
+					})}
+				/>
+				{errors?.name?.message && <Error>{errors.name.message}</Error>}
+			</FormRow2>
 
-      <FormRow>
-        <Label htmlFor="description">Description for website</Label>
-        <Textarea type="number" id="description" defaultValue="" {...register("description")} />
-      </FormRow>
+			<FormRow label="Cabin name" error={errors?.name?.message}>
+				<Input
+					type="text"
+					id="name"
+					{...register("name", {
+						required: "This field is required",
+					})}
+				/>
+			</FormRow>
 
-      <FormRow>
-        <Label htmlFor="image">Cabin photo</Label>
-        <FileInput id="image" accept="image/*" />
-      </FormRow>
+			<FormRow2>
+				<Label htmlFor="maxCapacity">Maximum capacity</Label>
+				<Input
+					type="number"
+					id="maxCapacity"
+					{...register("maxCapacity", {
+						required: "This field is required",
+						min: {
+							value: 1,
+							message: "Capacity should be at least 1",
+						},
+					})}
+				/>
+			</FormRow2>
 
-      <FormRow>
-        {/* type is an HTML attribute! */}
-        <Button variation="secondary" type="reset">
-          Cancel
-        </Button>
-        <Button disabled={isCreating}>Edit cabin</Button>
-      </FormRow>
-    </Form>
-  );
+			<FormRow2>
+				<Label htmlFor="regularPrice">Regular price</Label>
+				<Input
+					type="number"
+					id="regularPrice"
+					{...register("regularPrice", {
+						required: "This field is required",
+						min: {
+							value: 1,
+							message: "Capacity should be at least 1",
+						},
+					})}
+				/>
+			</FormRow2>
+
+			<FormRow2>
+				<Label htmlFor="discount">Discount</Label>
+				<Input
+					type="number"
+					id="discount"
+					defaultValue={0}
+					{...register("discount", {
+						required: "This field is required",
+						validate: (value) =>
+							value <= getValues().regularPrice ||
+							"Discount should be less than regular price",
+					})}
+				/>
+			</FormRow2>
+
+			<FormRow2>
+				<Label htmlFor="description">Description for website</Label>
+				<Textarea
+					type="number"
+					id="description"
+					defaultValue=""
+					{...register("description", {
+						required: "This field is required",
+					})}
+				/>
+			</FormRow2>
+
+			<FormRow2>
+				<Label htmlFor="image">Cabin photo</Label>
+				<FileInput id="image" accept="image/*" />
+			</FormRow2>
+
+			<FormRow2>
+				{/* type is an HTML attribute! */}
+				<Button variation="secondary" type="reset">
+					Cancel
+				</Button>
+				<Button disabled={isCreating}>Edit cabin</Button>
+			</FormRow2>
+		</Form>
+	);
 }
 
 export default CreateCabinForm;
