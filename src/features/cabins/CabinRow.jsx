f924912ecb647143988035c6505e0a28630ea505
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2024-12-31 12:16:07 +0000

    Done Building a Reuseable Context Menu

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -89,19 +89,24 @@ function CabinRow({ cabin }) {
 			)}
 			<div>
 				<Modal>
-					<Modal.Open opens="edit">
-						<button>
-							<HiPencil />
-						</button>
+				<Menus.Menu>
+					<Menus.Toggle id={cabinId} />
+
+					<Menus.List id={cabinId}>
+						<Menus.Button icon={<HiSquare2Stack />} onClick={handleDuplicate}>Duplicate</Menus.Button>
+						<Modal.Open opens="edit">
+					<Menus.Button icon={<HiPencil />}>Edit</Menus.Button>
 					</Modal.Open>
+					<Modal.Open opens="delete">
+					<Menus.Button icon={<HiTrash />}>Delete</Menus.Button>
+					</Modal.Open>
+						</Menus.List>
+						
+					
 					<Modal.Window name="edit">
 						<CreateCabinForm cabinToEdit={cabin} />
 					</Modal.Window>
-					<Modal.Open opens="delete">
-						<button>
-							<HiTrash />
-						</button>
-					</Modal.Open>
+					
 					<Modal.Window name="delete">
 						<ConfirmDelete
 							resourceName="cabins"
@@ -109,17 +114,10 @@ function CabinRow({ cabin }) {
 							onConfirm={() => deleteCabin(cabinId)}
 						/>
 					</Modal.Window>
+				</Menus.Menu>
 				</Modal>
 
-				<Menus.Menu>
-					<Menus.Toggle id={cabinId} />
-
-					<Menus.List id={cabinId}>
-						<Menus.Button icon={<HiSquare2Stack />} onClick={handleDuplicate}>Duplicate</Menus.Button>
-						<Menus.Button icon={<HiPencil />}>Edit</Menus.Button>
-						<Menus.Button icon={<HiTrash />}>Delete</Menus.Button>
-					</Menus.List>
-				</Menus.Menu>
+				
 			</div>
 		</Table.Row>
 	);
