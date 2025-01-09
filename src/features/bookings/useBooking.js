b97c7f244cea7d5917797855190374b9fce04cc1
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2025-01-09 13:46:49 +0000

    done building the single booking page

---
+++ src/features/bookings/useBooking.js
@@ -0,0 +1,23 @@
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+
+import Booking from "../../pages/Booking";
+import { getBooking } from "../../services/apiBookings";
+
+export function useBooking() {
+	const { bookingId } = useParams();
+
+	const {
+		isLoading,
+		data: booking,
+		error,
+	} = useQuery({
+		queryKey: ["booking"],
+		queryFn: () => getBooking(bookingId),
+		retry: false,
+	});
+
+	
+
+return { isLoading, error, booking } ;
+}
