db9b66419c7d5a4e372c19fcfeff702b4b50939d
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2025-01-09 11:40:47 +0000

    Building the single booking page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import GlobalStyles from "./styles/GlobalStyles";
 import Dashboard from "./pages/Dashboard";
 import Bookings from "./pages/Bookings";
+import Booking from "./pages/Booking";
 import Cabins from "./pages/Cabins";
 import Users from "./pages/Users";
 import Settings from "./pages/Settings";
@@ -34,6 +35,7 @@ function App() {
 						<Route index element={<Navigate replace to="dashboard" />} />
 						<Route path="dashboard" element={<Dashboard />} />
 						<Route path="bookings" element={<Bookings />} />
+						<Route path="bookings/:bookingId" element={<Booking />} />
 						<Route path="cabins" element={<Cabins />} />
 						<Route path="users" element={<Users />} />
 						<Route path="settings" element={<Settings />} />
---
+++ src/pages/Booking.jsx
@@ -0,0 +1,7 @@
+function Booking() {
+    return (
+        <div>Booking detail</div>
+    )
+}
+
+export default Booking;
