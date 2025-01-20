f7e6755c27b5c25a83925f0db3afd409835f8d05
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2025-01-20 11:19:46 +0000

    Done with Authenticating User Login with Supabase

--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -20,7 +20,14 @@ const Heading = styled.h1`
 
     ${(props) => props.as === "h3" && css`
         font-size: 2rem;
+        font-weight: 500;
+        /* background-color: yellow; */
+    `}
+
+    ${(props) => props.as === "h4" && css`
+        font-size: 3rem;
         font-weight: 600;
+        text-align: center;
         /* background-color: yellow; */
     `}
 
